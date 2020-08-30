4b057d21249162f831a4600b36a43d97a8447e85
Ryan Heard <ryanwheard@gmail.com>
2020-08-29 19:05:49 -0500

    Add WiindowsBase required reference

--- netauto/__init__.py
+++ netauto/__init__.py
@@ -4,6 +4,8 @@ from clr import System
 from expanded_clr.utils import python_name_to_csharp_name
 
 
+# TODO: Build a searching mechanism (is there one in pythonnet or C#) so these don't need to be in the cwd.
+clr.AddReference('WindowsBase')
 clr.AddReference('UIAutomationClient')
 
 
