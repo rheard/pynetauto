3984375c1cca3e857df263dd7347ad2f234a8f52
Ryan Heard <ryanwheard@gmail.com>
2020-09-19 20:09:33 -0500

    remove reference

--- netauto/__init__.py
+++ netauto/__init__.py
@@ -5,7 +5,6 @@ from expanded_clr.utils import python_name_to_csharp_name
 
 
 # TODO: Build a searching mechanism (is there one in pythonnet or C#) so these don't need to be in the cwd.
-clr.AddReference('WindowsBase')
 clr.AddReference('UIAutomationClient')
 
 
